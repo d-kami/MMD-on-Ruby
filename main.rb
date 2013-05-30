265909aa4cc24998b57708f035ed0beb8df28cde
d-kami <kami229@hotmail.com>
2013-05-30 15:22:42 +0900

    add comment to main.rb

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 require 'opengl'
 require 'glut'
 require './mmd.rb'
@@ -5,30 +7,43 @@ require './motion.rb'
 require './bmp.rb'
 require './pureimage.rb'
 
+#読み込むモデルファイルのファイル名
 model_file = 'mikumetal.pmd'
+
+#読み込むモーションファイルのファイル名
 motion_file = 'kishimen.vmd'
+
+#読み込む頂点シェーダとフラグメントシェーダ
 shader_file = ['mmd.vert', 'mmd.frag']
 
 class Object3D
+
+    #toonファイルの名前を配列にセットする
     def set_toon_names()
         @toons = Array.new()
+        
+        #toon_indexが-1のときのtoonファイル
         @toons[10] = 'toon00.bmp'
     
         if @model.toon_texture == nil
+            #toonファイルの名前に指定が無い場合はデフォルトのファイル名を使う
             10.times do |index|
                 @toons[index] = get_default_toon(index)
             end
         else
             10.times do |index|
                 if @model.toon_texture.names[index] != nil && @model.toon_texture.names[index].end_with?('.bmp')
+                    #toonファイルの名前に指定がある
                     @toons[index] = @model.toon_texture.names[index]
                 else
+                    #toonファイルの名前に指定が無いのでデフォルトのファイル名を使う
                     @toons[index] = get_default_toon(index)
                 end
             end
         end
     end
     
+    #index番目のデフォルトのファイル名を返す
     def get_default_toon(index)
         if index == 9
             return "toon#{index + 1}.bmp"
@@ -37,6 +52,7 @@ class Object3D
         end
     end
 
+    #toonファイルを読み込み、テクスチャとして設定する
     def load_toons()
         @toons.length.times do |index|
             bitmap = BitMap.read("./toon/#{@toons[index]}")
@@ -46,31 +62,39 @@ class Object3D
         end
     end
     
+    #bmpを読み込み、テクスチャにして返す
     def load_bmp(file_name)
         bitmap = BitMap.read(file_name)
         image = get_raw(bitmap)
         return create_texture(image, bitmap.width, bitmap.height)
     end
     
+    #スフィアマップ用のテクスチャを読み込み設定する
     def load_sphere(material)
         if material.sphere != nil && !@textures.key?(material.sphere)
             @textures[material.sphere] = load_bmp("./model/#{material.sphere}")
         end
     end
     
+    #マテリアルに設定されているテクスチャを読み込む
     def load_texture(material)
         if material.texture != nil && !@textures.key?(material.texture)
+            #読み込むテクスチャが存在していて、まだ登録されてない場合の処理
+            
             if material.texture.end_with?('.bmp')
+                #bmpファイルをテクスチャとする
                 @textures[material.texture] = load_bmp("./model/#{material.texture}")
             elsif material.texture.end_with?('.png')
                 pngio = PureImage::PNGIO.new()
                 png = pngio.load("./model/#{material.texture}")
                 image = get_raw_png(png)
+                #pngファイルをテクスチャとする
                 @textures[material.texture] = create_texture(image, png.width, png.height)
             end
         end
     end
 
+    #bitmapからRGB配列を取得する
     def get_raw(bitmap)
         image = ''
         
@@ -88,6 +112,7 @@ class Object3D
         return image
     end
     
+    #pngからRGB配列を取得する
     def get_raw_png(png)
         image = ''
         
@@ -105,6 +130,7 @@ class Object3D
         return image
     end
     
+    #テクスチャを作成して返す
     def create_texture(image, width, height)
         texture = GL.GenTextures(1)[0]
         
@@ -122,23 +148,29 @@ class Object3D
         return texture
     end
 
+    #MMDのモデルファイルを読み込む
     def load_model(file_name)
         File.open(file_name, 'rb'){|file|
             @model = MMDModel.new()
             @model.load(file)
             @textures = Hash.new()
 
+            #マテリアルごとにスフィアマップ画像、テクスチャ画像を読み込む
             @model.materials.each do |material|
                 load_sphere(material)
                 load_texture(material)
             end
 
+            #toonファイル名を設定し読み込む
             set_toon_names()
             load_toons()
+            
+            #表情を連想配列で管理するように設定する
             set_skins()
         }
     end
     
+    #表情を連想配列で管理するように設定する
     def set_skins()
         @skin_map = Hash.new()
     
@@ -147,11 +179,13 @@ class Object3D
         }
     end
     
+    #MMDのモーションファイル(VMD)を読み込む
     def load_motion(file_name)
         File.open(file_name, 'rb'){|file|
             @motion = MMDMotion.new()
             @motion.load(file)
             
+            #表情の順番をflame_noでソートする
             @motion.skins.sort! do |skin1, skin2|
                 skin1.flame_no <=> skin2.flame_no
             end
@@ -161,11 +195,13 @@ class Object3D
         }
     end
     
+    #光源の色と方向設定
     def init_light()
         @light_diffuse = [1.0, 1.0, 1.0]
         @light_dir = [0.5, 1.0, 0.5]
     end
 
+    #描画領域のサイズの変更
     def reshape(w,h)
         GL.Viewport(0, 0, w, h)
 
@@ -174,15 +210,19 @@ class Object3D
         GLU.Perspective(45.0, w.to_f() / h.to_f(), 0.1, 100.0)
     end
     
+    #表情の変更を行う
     def skin_motions()
         while @skin_index < @motion.skins.length && @motion.skins[@skin_index].flame_no == @frame
+            #@frameと一致しているflame_noを持つ表情を全て設定する
             skin_motion()
 
             @skin_index += 1
         end
     end
     
+    #表情の変更を行う
     def skin_motion()
+        #モデルに登録されてない表情は無視する
         if !@skin_map.key?(@motion.skins[@skin_index].name)
             return
         end
@@ -190,12 +230,14 @@ class Object3D
         skin = @skin_map[@motion.skins[@skin_index].name]
 
         if skin.name == 'base'
+            #デフォルトの表情
             skin.vertices.each do |base|
                 3.times{|i|
                     @model.vertices[base.index].pos[i] = base.pos[i]
                 }
             end
         else
+            #変更する表情
             skin.vertices.each do |vertex|
                 base = @model.skins[0].vertices[vertex.index]
 
@@ -206,26 +248,35 @@ class Object3D
         end
     end
 
+    #モデル情報の更新を行う
     def update_motion()
         skin_motions()
     end
 
+    #3D画面の描画を行う
     def display()
         GL.UseProgram(@program)
 
+        #カメラの設定
         GL.MatrixMode(GL::GL_MODELVIEW)
         GL.LoadIdentity()
         GLU.LookAt(0.0, 0.0, 37.0, 0.0, 10.0, 0.0, 0.0, 1.0, 0.0)
 
+        #背景色の設定
         GL.ClearColor(0.0, 0.0, 1.0, 1.0)
         GL.Clear(GL::GL_COLOR_BUFFER_BIT | GL::GL_DEPTH_BUFFER_BIT)
 
+        #モデルの回転
         GL.Rotate(@rotX, 1, 0, 0)
         GL.Rotate(@rotY, 0, 1, 0)
 
+        #カリングを有効にする
         GL.Enable(GL::CULL_FACE)
 
+        #モデル描画
         draw_model()
+        
+        #エッジ描画
         draw_edge()
         
         GLUT.SwapBuffers()
@@ -233,32 +284,43 @@ class Object3D
         @frame += 1
     end
     
+    #モデル描画
     def draw_model()
         GL.CullFace(GL::BACK)
         GL.Uniform1i(@locations[:is_edge], 0)
-        
+
         GL.Enable(GL::DEPTH_TEST)
         GL.DepthFunc(GL::LEQUAL)
+        
+        #アルファブレンドを有効にする
         GL.Enable(GL::BLEND)
         GL.BlendFuncSeparate(GL::SRC_ALPHA, GL::ONE_MINUS_SRC_ALPHA, GL::SRC_ALPHA, GL::DST_ALPHA)
 
+        #マテリアルの開始番号
         start = 0
 
+        #マテリアルを1つずつ描画する
         @model.materials.each do |material|
+            #モデルの一部分を描画する
             draw_part(material, start)
             start += material.vert_count
         end
     end
     
+    #エッジを描画する
     def draw_edge()
+        #アルファブレンドを無効にする
         GL.Disable(GL::BLEND)
         GL.CullFace(GL::FRONT)
         GL.Uniform1i(@locations[:is_edge], 1)
 
+        #マテリアルの開始番号
         start = 0
 
         @model.materials.each do |material|
+            #エッジとなるマテリアルのみ描画する
             if(material.edge_flag)
+                #モデルの一部分を描画する
                 draw_part(material, start)
             end
             
@@ -266,20 +328,33 @@ class Object3D
         end
     end
     
+    #モデルの一部分を描画する
     def draw_part(material, start)
+        #テクスチャの準備、設定
         prepare_texture(material)
+        
+        #スフィアマップの準備、設定
         prepare_sphere(material)
+        
+        #toonテクスチャの準備、設定
         prepare_toon(material)
+        
+        #materialの設定
         prepare_material(material)
+        
+        #光源の設定
         prepare_light()
         
+        #マテリアルの描画
         draw_material(material, start)
     end
     
+    #テクスチャの準備、設定
     def prepare_texture(material)
         useTexture = 0
     
         if material.texture != nil && material.texture.length > 0
+            #マテリアルに貼るテクスチャがある場合はテクスチャ情報をシェーダに渡す
             GL.ActiveTexture(GL::TEXTURE0)
             GL.BindTexture(GL::TEXTURE_2D, @textures[material.texture])
             GL.Uniform1i(@locations[:sampler], 0)
@@ -290,13 +365,16 @@ class Object3D
         GL.Uniform1i(@locations[:use_texture], useTexture)
     end
     
+    #スフィアマップの準備、設定
     def prepare_sphere(material)
         if material.sphere != nil && material.sphere.length > 0
+            #マテリアルに貼るスフィアマップ用テクスチャがある場合はスフィアマップ用テクスチャ情報をシェーダに渡す
             GL.ActiveTexture(GL::TEXTURE2)
             GL.BindTexture(GL::TEXTURE_2D, @textures[material.sphere])
             GL.Uniform1i(@locations[:is_sphere_use], 1)
             GL.Uniform1i(@locations[:sphere_sampler], 2)
             
+            #スフィアマップの設定。1なら加算、0なら乗算
             if material.sphere.end_with?('.spa')
                 GL.Uniform1i(@locations[:is_sphere_add], 1)
             else
@@ -307,6 +385,7 @@ class Object3D
         end
     end
 
+    #toonテクスチャの準備、設定
     def prepare_toon(material)
         toon_index = material.toon_index
 
@@ -314,11 +393,13 @@ class Object3D
             toon_index = 10
         end
 
+        #toonテクスチャの情報をシェーダに渡す
         GL.ActiveTexture(GL::TEXTURE1)
         GL.BindTexture(GL::TEXTURE_2D, @textures[@toons[toon_index]])
         GL.Uniform1i(@locations[:toon_sampler], 1)
     end
     
+    #materialの設定
     def prepare_material(material)
         GL.Uniform1f(@locations[:alpha], material.alpha)
         GL.Uniform3fv(@locations[:ambient], material.ambient)
@@ -327,21 +408,25 @@ class Object3D
         GL.Color(material.diffuse[0], material.diffuse[1], material.diffuse[2])
     end
     
+    #光源の設定
     def prepare_light
         GL.Uniform3fv(@locations[:light_dir], @light_dir)
         GL.Uniform3fv(@locations[:light_diffuse], @light_diffuse)
     end
 
+    #マテリアルの描画
     def draw_material(material, start)
         GL.Begin(GL::TRIANGLES)
 
         material.vert_count.times do |findex|
+            #1頂点ずつ情報を取得
             vindex = @model.face.indices[start + findex]
             vertex = @model.vertices[vindex]
             pos = vertex.pos
             normal = vertex.normal
             uv = vertex.uv
             
+            #1頂点ずつOpenGLに情報を渡す
             GL.TexCoord2f(uv[0], uv[1])
             GL.Normal(normal[0], normal[1], normal[2])
             GL.Vertex(pos[0], pos[1], pos[2])
@@ -349,9 +434,11 @@ class Object3D
 
         GL.End()
         
+        #テクスチャ解除
         GL.BindTexture(GL::TEXTURE_2D, 0)
     end
 
+    #マウスのボタンが押されたときと離されたときのイベント
     def mouse(button, state, x, y)
         if button == GLUT::GLUT_LEFT_BUTTON && state == GLUT::GLUT_DOWN then
             @start_x = x
@@ -362,7 +449,9 @@ class Object3D
         end
     end
 
+    #マウスを移動したときのイベント
     def motion(x, y)
+        #ドラッグ中なら、モデルを回転する
         if @drag_flg then
             dx = x - @start_x
             dy = y - @start_y
@@ -379,14 +468,17 @@ class Object3D
         GLUT.PostRedisplay()
     end
 
+    #1フレームごとに呼び出されるメソッド
     def update(value)
         GLUT.TimerFunc(33 , method(:update).to_proc(), 0)
         
         update_motion()
         
+        #再描画要求
         GLUT.PostRedisplay()
     end
 
+    #初期化
     def initialize()
         @start_x = 0
         @start_y = 0
@@ -397,11 +489,13 @@ class Object3D
         init_light()
     end
     
+    #モデルとモーションの読み込み
     def load(model_name, motion_name)
         load_model("./model/#{model_name}")
         load_motion("./motion/#{motion_name}")
     end
     
+    #シェーダの読み込み、コンパイル、Uniform変数へのリンク
     def load_shader(vert_shader, frag_shader)
         @program = create_program("./shader/#{vert_shader}", "./shader/#{frag_shader}")
         
@@ -421,14 +515,7 @@ class Object3D
         @locations[:sphere_sampler] = GL.GetUniformLocation(@program, 'sphereSampler')
     end
     
-    def set_func()
-        GLUT.ReshapeFunc(method(:reshape).to_proc())
-        GLUT.DisplayFunc(method(:display).to_proc())
-        GLUT.MouseFunc(method(:mouse).to_proc())
-        GLUT.MotionFunc(method(:motion).to_proc())
-        GLUT.TimerFunc(33 , method(:update).to_proc(), 0)
-    end
-    
+    #シェーダをコンパイルして、リンクする
     def create_program(vert_name, frag_name)
         program = GL.CreateProgram()
         
@@ -449,6 +536,7 @@ class Object3D
         return program
     end
 
+    #シェーダのコンパイルする
     def create_shader(file_name, type)
         shader = GL.CreateShader(type)
         
@@ -464,6 +552,7 @@ class Object3D
         return shader
     end
     
+    #OpenGL用のパラメータを設定
     def set_gl_parameter()
         GL.Enable(GL::GL_AUTO_NORMAL)
         GL.Enable(GL::GL_NORMALIZE)
@@ -472,12 +561,22 @@ class Object3D
         GL.DepthFunc(GL::GL_LESS)
     end
     
-    def create_window(x, y, width, height)
+    #GLUTでの初期化
+    def create_window(x, y, width, height, title)
         GLUT.InitWindowPosition(x, y)
         GLUT.InitWindowSize(width, height)
         GLUT.Init()
         GLUT.InitDisplayMode(GLUT::GLUT_DOUBLE | GLUT::GLUT_RGB | GLUT::GLUT_DEPTH)
-        GLUT.CreateWindow('MMD on Ruby')
+        GLUT.CreateWindow(title)
+    end
+    
+    #イベントとメソッドを関連付ける
+    def set_func()
+        GLUT.ReshapeFunc(method(:reshape).to_proc())
+        GLUT.DisplayFunc(method(:display).to_proc())
+        GLUT.MouseFunc(method(:mouse).to_proc())
+        GLUT.MotionFunc(method(:motion).to_proc())
+        GLUT.TimerFunc(33 , method(:update).to_proc(), 0)
     end
 
     def start()
@@ -486,7 +585,7 @@ class Object3D
 end
 
 object = Object3D.new()
-object.create_window(100, 100, 450, 450)
+object.create_window(100, 100, 450, 450, 'MMD on Ruby')
 object.set_gl_parameter()
 
 object.load(model_file, motion_file)
